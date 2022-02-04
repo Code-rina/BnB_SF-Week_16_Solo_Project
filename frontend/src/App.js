2fd1fc2967d3307a7fd81fa880ad085db18ed966
Code-rina <code-rina@Katkas-MacBook-Air.local>
2022-02-03 20:28:52 -0800

    signup modal complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormModal from "./components/SignupFormModal";
-import * as sessionActions from "./store/session";
+import { Switch } from 'react-router-dom'
 import Navigation from "./components/Navigation";
+import { useDispatch } from 'react-redux'
+import * as sessionActions from "./store/session";
+
+
 
 function App() {
   const dispatch = useDispatch();
@@ -12,16 +13,12 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return (
+
+  return isLoaded && (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/signup">
-            <SignupFormModal />
-          </Route>
-        </Switch>
-      )}
+      <Switch>
+      </Switch>
     </>
   );
 }
