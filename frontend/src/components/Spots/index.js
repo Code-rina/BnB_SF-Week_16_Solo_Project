d529d61847ffad5f316c8a62ea6888a670b55b4c
Code-rina <code-rina@Katkas-MacBook-Air.local>
2022-02-08 10:41:19 -0800

    spots not fully functioning

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 // import { useState } from 'react';
 // import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import './spots.css';
 
 
 function Spots(){
 
-        const dispatch = useDispatch()
-        const spotsAll = useSelector(state => state.spots.list)
 
     return (
         <div className="main_spots_container">
