f06f5b3a1727a0f6f9f6ad229281288fcc89953a
Code-rina <code-rina@Katkas-MacBook-Air.local>
2022-02-08 09:30:59 -0800

    created spots route

--- frontend/src/components/EditHostForm/index.js
+++ frontend/src/components/EditHostForm/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 // import { useSelector } from 'react-redux';
 // import { useState } from 'react';
 // import { Link } from 'react-router-dom';
-// import './spotdetail.css';
+import './edithostform.css';
 
 
-function SpotDetail(){
+function EditHostForm(){
 //   const sessionUser = useSelector(state => state.session.user);
     return (
-        <div className="main_spotdetail_container">
+        <div className="main_edithostform_container">
             
         </div>
     )
 }
 
-export default SpotDetail;
+export default EditHostForm;
---
+++ frontend/src/store/spots.js
@@ -0,0 +1,23 @@
+export const LOAD_SPOTS = "spots/LOAD_SPOTS"
+export const ADD_SPOT = "spots/ADD_SPOT"
+export const UPDATE_SPOT = "spots/UPDATE_SPOT"
+// export const REMOVE_SPOT = "spots/REMOVE_SPOT"
+
+//  const loadSpot = (spots) => ({
+//     type: LOAD_SPOTS,
+//     spots,
+//  });
+
+//  const addSpot = (spot) => ({
+//      type: ADD_SPOT,
+//      spot,
+//  });
+
+//  const updateSpot = (spot) => ({
+//      type: UPDATE_SPOT,
+//      spot,
+//  });
+//  const removeSpot = (spot) => ({
+//      type: REMOVE_SPOT,
+//      spot
+//  })
