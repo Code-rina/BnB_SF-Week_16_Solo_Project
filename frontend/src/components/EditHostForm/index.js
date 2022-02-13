b59cb0c2a27911614a949dae424bc101dfe617ee
Code-rina <code-rina@Katkas-MacBook-Air.local>
2022-02-13 02:31:40 -0800

    edit for updated and pushing to heroku

--- frontend/src/components/EditHostForm/index.js
+++ frontend/src/components/EditHostForm/index.js
@@ -123,202 +123,207 @@ function EditHostForm(){
     
 
     return (
-        <div className="host_edit_form">
-            <form onSubmit={handleSubmit}>
-            <div id="new_form_container">
-                <h3>Host Edit Form</h3>
-      <ul>
-        {errorValidator.map(error => (
-          <li className="error_list" key={error}>{error}</li>
-        ))}
-      </ul>
+      <div className="host_form">
+      <form onSubmit={handleSubmit}>
+      <div id="form_container">
+        <div id="host_form_top">
+          <h3 id="top_title">Become a Host</h3>
+<ul id="errors_form">
+  {errorValidator.map(error => (
+    <li className="error_list" key={error}>{error}</li>
+  ))}
+</ul>
+</div>
+<div id="host_form_left">
+<label id="form_label_address">
+  Address
+  <input id="form_address_text_input"
+    type="text"
+    placeholder="Address here..."
+    value={address}
+    onChange={(e) => setAddress(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_city">
+  City
+  <input id="form_city_text_input"
+    type="text"
+    placeholder="City here ..."
+    value={city}
+    onChange={(e) => setCity(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_zipCode">
+  Zip Code
+  <input id="form_zipCode_text_input"
+    type="text"
+    placeholder="Zip Code here ..."
+    value={zipCode}
+    onChange={(e) => setZipCode(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_state">
+  State
+  <input id="form_state_text_input"
+    type="text"
+    placeholder="State here ..."
+    value={state}
+    onChange={(e) => setState(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_country">
+  Country
+  <input id="form_country_text_input"
+    type="text"
+    placeholder="Country here ..."
+    value={country}
+    onChange={(e) => setCountry(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_title">
+  Title
+  <input id="form_title_text_input"
+    type="text"
+    placeholder="Title here ..."
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_description">
+  Description
+  <input id="form_description_text_input"
+    type="text"
+    placeholder="Description here ..."
+    value={description}
+    onChange={(e) => setDescription(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_price">
+  Price
+  <input id="form_price_text_input"
+    type="text"
+    placeholder="Price here ..."
+    value={price}
+    onChange={(e) => setPrice(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_guests">
+  Guests
+  <input id="form_guests_text_input"
+    type="text"
+    placeholder="Guests here ..."
+    value={guests}
+    onChange={(e) => setGuests(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_bedrooms">
+  Bedrooms
+  <input id="form_bedrooms_text_input"
+    type="text"
+    placeholder="Bedrooms here ..."
+    value={bedrooms}
+    onChange={(e) => setBedrooms(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_bathrooms">
+  Bathrooms
+  <input id="form_bathrooms_text_input"
+    type="text"
+    placeholder="Bathrooms here ..."
+    value={bathrooms}
+    onChange={(e) => setBathrooms(e.target.value)}
+    required
+  />
+</label>
+<label id="form_label_url">
+  Image Url
+  <input id="form_url_text_input"
+    type="text"
+    placeholder="Url here ..."
+    value={url}
+    onChange={(e) => setUrl(e.target.value)}
+    required
+  />
+</label>
+<div className="checkboxes">
+<label id="form_label_kitchen">
+  Kitchen
+  <input id="form_kitchen_text_input"
+    type="checkbox"
+    value={kitchen}
+    onChange={(e) => setKitchen(!kitchen)}
+  />
+</label>
+<label id="form_label_parking">
+  Parking
+  <input id="form_parking_text_input"
+    type="checkbox"
+    value={parking}
+    onChange={(e) => setParking(!parking)}
+  />
+</label>
+<label id="form_label_patio">
+  Patio
+  <input id="form_patio_text_input"
+    type="checkbox"
+    value={patio}
+    onChange={(e) => setPatio(!patio)}
+  />
+</label>
+<label id="form_label_pool">
+  Pool
+  <input id="form_pool_text_input"
+    type="checkbox"
+    value={pool}
+    onChange={(e) => setPool(!pool)}
+  />
+</label>
+<label id="form_label_gym">
+  Gym
+  <input id="form_gym_text_input"
+    type="checkbox"
+    value={gym}
+    onChange={(e) => setGym(!gym)}
+  />
+</label>
+<label id="form_label_hottub">
+  Hot Tub
+  <input id="form_hottub_text_input"
+    type="checkbox"
+    value={hotTub}
+    onChange={(e) => setHotTub(!hotTub)}
+  />
+</label>
+<label id="form_label_pets">
+  Pets
+  <input id="form_pets_text_input"
+    type="checkbox"
+    value={pets}
+    onChange={(e) => setPets(!pets)}
+  />
+</label>
+</div>
+<button id="submit_form_button" 
+type="submit" 
+disabled={errorValidator.length > 0}>Submit</button>
+      <Link id="cancel_form_button" exact="true" to="/">Cancel</Link>
       </div>
-      <label id="new_form_label_address">
-        Address
-        <input id="new_form_address_text_input"
-          type="text"
-          placeholder="Address here..."
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_city">
-        City
-        <input id="new_form_city_text_input"
-          type="text"
-          placeholder="City here ..."
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_zipCode">
-        Zip Code
-        <input id="new_form_zipCode_text_input"
-          type="text"
-          placeholder="Zip Code here ..."
-          value={zipCode}
-          onChange={(e) => setZipCode(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_state">
-        State
-        <input id="new_form_state_text_input"
-          type="text"
-          placeholder="State here ..."
-          value={state}
-          onChange={(e) => setState(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_country">
-        Country
-        <input id="new_form_country_text_input"
-          type="text"
-          placeholder="Country here ..."
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_title">
-        Title
-        <input id="new_form_title_text_input"
-          type="text"
-          placeholder="Title here ..."
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_description">
-        Description
-        <input id="new_form_description_text_input"
-          type="text"
-          placeholder="Description here ..."
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_price">
-        Price
-        <input id="new_form_price_text_input"
-          type="text"
-          placeholder="Price here ..."
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_guests">
-        Guests
-        <input id="new_form_guests_text_input"
-          type="text"
-          placeholder="Guests here ..."
-          value={guests}
-          onChange={(e) => setGuests(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_bedrooms">
-        Bedrooms
-        <input id="new_form_bedrooms_text_input"
-          type="text"
-          placeholder="Bedrooms here ..."
-          value={bedrooms}
-          onChange={(e) => setBedrooms(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_bathrooms">
-        Bathrooms
-        <input id="new_form_bathrooms_text_input"
-          type="text"
-          placeholder="Bathrooms here ..."
-          value={bathrooms}
-          onChange={(e) => setBathrooms(e.target.value)}
-          required
-        />
-      </label>
-      <label id="new_form_label_url">
-        Image Url
-        <input id="new_form_url_text_input"
-          type="text"
-          placeholder="Url here ..."
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          required
-        />
-      </label>
-      <div className="checkboxes">
-      <label id="new_form_label_kitchen">
-        Kitchen
-        <input id="new_form_kitchen_text_input"
-          type="checkbox"
-          value={kitchen}
-          onChange={(e) => setKitchen(!kitchen)}
-        />
-      </label>
-      <label id="new_form_label_parking">
-        Parking
-        <input id="new_form_parking_text_input"
-          type="checkbox"
-          value={parking}
-          onChange={(e) => setParking(!parking)}
-        />
-      </label>
-      <label id="new_form_label_patio">
-        Patio
-        <input id="new_form_patio_text_input"
-          type="checkbox"
-          value={patio}
-          onChange={(e) => setPatio(!patio)}
-        />
-      </label>
-      <label id="new_form_label_pool">
-        Pool
-        <input id="new_form_pool_text_input"
-          type="checkbox"
-          value={pool}
-          onChange={(e) => setPool(!pool)}
-        />
-      </label>
-      <label id="new_form_label_gym">
-        Gym
-        <input id="new_form_gym_text_input"
-          type="checkbox"
-          value={gym}
-          onChange={(e) => setGym(!gym)}
-        />
-      </label>
-      <label id="new_form_label_hottub">
-        Hot Tub
-        <input id="new_form_hottub_text_input"
-          type="checkbox"
-          value={hotTub}
-          onChange={(e) => setHotTub(!hotTub)}
-        />
-      </label>
-      <label id="new_form_label_pets">
-        Pets
-        <input id="new_form_pets_text_input"
-          type="checkbox"
-          value={pets}
-          onChange={(e) => setPets(!pets)}
-        />
-      </label>
       </div>
-      <button id="new_submit_form_button" 
-      type="submit" 
-      disabled={errorValidator.length > 0}>Submit</button>
-            <Link id="new_cancel_form_button" exact="true" to={`/spots/${spotId}`}>Cancel</Link>
-            </form>
-            </div>
-  );
-      
+      </form>
+      </div>
+   
+);
+
 }
 
 export default EditHostForm;
