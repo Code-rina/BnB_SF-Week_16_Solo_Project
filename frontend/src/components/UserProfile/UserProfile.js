b37201a78a3175f9409d4852bfae0954d87dec1e
Code-rina <katerina.kreibich@gmail.com>
2022-04-21 23:38:43 -0700

    get bookings almost complete

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -16,6 +16,9 @@ function UserProfile() {
     const [user, setUser] = useState({});
     const oneUser = useSelector((state) => state?.session?.user)
     const allSpots = useSelector((state) => state?.spots?.list)
+    console.log("allSpots---------", allSpots)
+    // const spots = useSelector((state) => state?.spots)
+    // console.log("spots---------", spots)
     const { id }  = useParams();
     //   console.log("allSpots!!!!!", allSpots)
     //   console.log("oneUser!!!!!", oneUser)
@@ -38,20 +41,22 @@ function UserProfile() {
     // console.log("TODAY:::::", today.format());
     
     const pastAndFutureBookings = (pastAndFutureBooking) => {
-        const today = moment()
+        const today = moment().format()
+        console.log("today-----", today)
         let pastBookings = []
         let futureBookings = []
-        allBookings.filter((i) => {
-            let singleBooking = allBookings[i];
+        allBookingsArray.forEach((singleBooking) => {
+            console.log("singleBooking-------------", singleBooking.startDate < today)
         if(singleBooking.startDate < today) {
             pastBookings.push([singleBooking.startDate, singleBooking])
         } else if (singleBooking.startDate > today) {
             futureBookings.push([singleBooking.startDate, singleBooking])
         }
         })
+        console.log("{ pastBookings: pastBookings, futureBookings: futureBookings}", { pastBookings: pastBookings, futureBookings: futureBookings})
         return { pastBookings: pastBookings, futureBookings: futureBookings}
     }
-
+    console.log("userBookings && pastAndFutureBookings(userBookings).futureBookings", userBookings && pastAndFutureBookings(userBookings).futureBookings)
     // const userBookings = allBookings.filter((oneBooking) => oneBooking?.userId === +oneUser.id)
     // console.log("userBookings!!!!!", userBookings)
     const [none, setNone] = useState(false)
@@ -91,18 +96,18 @@ function UserProfile() {
 
   return (
     <div className="profile-main-div">
-      <div className="profile-upper-div">
+        <div className="profile-upper-div">
             {/* <div className="profile-upper-head"> */}
-                <div className="profile-user-icon">
-                    <div className="profile-icon" color="red">
-                        <img className="profile-picture" src="https://i.thecartoonist.me/thecartoonist-profile.png" alt="profile_picture"/>
-                    </div>
-                    <h3 className="profile-welcome">Nice to see you again, {oneUser?.username}!</h3>
-                    <div className="profile-above-ul-div">
-                    <div className="profile-username">Username: {oneUser?.username}</div>
-                    <div className="profile-email">Email: {oneUser?.email}</div>
-                    </div>
+            <div className="profile-user-icon">
+                <div className="profile-icon" color="red">
+                    <img className="profile-picture" src="https://i.thecartoonist.me/thecartoonist-profile.png" alt="profile_picture"/>
                 </div>
+                <h3 className="profile-welcome">Nice to see you again, {oneUser?.username}!</h3>
+                <div className="profile-above-ul-div">
+                <div className="profile-username">Username: {oneUser?.username}</div>
+                <div className="profile-email">Email: {oneUser?.email}</div>
+                </div>
+            </div>
             {/* </div> */}
           <h5 className="profile-your-listings">Your listings: </h5>
             <div className="profile-lower-div">
@@ -155,41 +160,83 @@ function UserProfile() {
                         <div className="profile-user-spots">
                             {userBookings && pastAndFutureBookings(userBookings).futureBookings.map(date => {
                                 <div className="profile-listing-spots-div">
-                                    <NavLink className="profile-nav" to={`/spots/${date[1].spotId}`}>
+                                    <NavLink className="profile-nav" to={`/spots/${date[0].spotId}`}>
                                         <div className="profile-spot-img-div">
                                             <img className="profile-booking-img"
                                             alt="booking"
-                                            src={`${allSpots[date[1].spotId]?.Images[0]?.url}`}
+                                            src={`${allSpots[date[0].spotId]?.Images[0]?.url}`}
                                             onError={(e) =>
                                                 (e.target.src = "https://i.gyazo.com/953eaecab771a2f8f4e514e5750531cb.jpg")} 
                                                 />
                                             <div className="profile-title-address">
                                                 <div className="profile-title-div">
-                                                    <p className="profile-title">{allSpots[date[1].spotId]?.title}</p>
+                                                    <p className="profile-title">{allSpots[date[0].spotId]?.title}</p>
                                                 </div>    
                                                 <div className="profile-address-div">
-                                                    <p className="profile-address">{allSpots[date[1].spotId]?.address},</p>
-                                            </div>
-                                            <div className="profile-address-div">
-                                                <p className="profile-address">{allSpots[date[1].spotId]?.city},</p>
-                                            </div>
-                                            <div className="profile-address-div">
-                                                <p className="profile-address">{allSpots[date[1].spotId]?.zipCode}</p>
-                                            </div>
-                                        </div>    
-                                    </div>
-                                </NavLink>
-                            </div>
+                                                    <p className="profile-address">{allSpots[date[0].spotId]?.address},</p>
+                                                </div>
+                                                <div className="profile-address-div">
+                                                    <p className="profile-address">{allSpots[date[0].spotId]?.city},</p>
+                                                </div>
+                                                <div className="profile-address-div">
+                                                    <p className="profile-address">{allSpots[date[0].spotId]?.zipCode}</p>
+                                                </div>
+                                            </div>    
+                                        </div>
+                                    </NavLink>
+                                </div>
                             })}
                         </div>
-                    ))}
+                    }
+                    </div>
+                </div> 
+            </div>
+            <h5 className="profile-your-listings">Where you've been: </h5>
+            <div className="profile-lower-div">
+            {/* {userSpots && } */}
+                <div className="profile-spots">
+                    <div className="profile-spots-div">
+                    {!pastAndFutureBookings(userBookings).pastBookings.length ?
+                        <div className="profile-user-spots">
+                            <h4>No upcoming trips</h4>
+                                <NavLink to={'/spots'}><FaSuitcase />Explore</NavLink>
+                        </div> :
+                        <div className="profile-user-spots">
+                            {userBookings && pastAndFutureBookings(userBookings).pastBookings.map(date => {
+                                <div className="profile-listing-spots-div">
+                                    <NavLink className="profile-nav" to={`/spots/${date[0].spotId}`}>
+                                        <div className="profile-spot-img-div">
+                                            <img className="profile-booking-img"
+                                            alt="booking"
+                                            src={`${allSpots[date[0].spotId]?.Images[0]?.url}`}
+                                            onError={(e) =>
+                                                (e.target.src = "https://i.gyazo.com/953eaecab771a2f8f4e514e5750531cb.jpg")} 
+                                                />
+                                            <div className="profile-title-address">
+                                                <div className="profile-title-div">
+                                                    <p className="profile-title">{allSpots[date[0].spotId]?.title}</p>
+                                                </div>    
+                                                <div className="profile-address-div">
+                                                    <p className="profile-address">{allSpots[date[0].spotId]?.address},</p>
+                                                </div>
+                                                <div className="profile-address-div">
+                                                    <p className="profile-address">{allSpots[date[0].spotId]?.city},</p>
+                                                </div>
+                                                <div className="profile-address-div">
+                                                    <p className="profile-address">{allSpots[date[0].spotId]?.zipCode}</p>
+                                                </div>
+                                            </div>    
+                                        </div>
+                                    </NavLink>
+                                </div>
+                            })}
+                        </div>
+                    }
                     </div>
                 </div>
             </div>
-                </div>
-            </div>
         </div>
-        </div>
+    </div>
   );
 }
 export default UserProfile;
